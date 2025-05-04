6c55a551155777ec43694ade90830fe514cce181
Raman <ramanpannu1003@gmail.com>
2025-05-04 05:56:19 +0000

    allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all interfaces
+    port: 3000,       // Set the port to 3000
+    strictPort: true, // Make sure it doesn't switch to another port if 3000 is in use
+    allowedHosts: [
+      'dev.ramxn.com',       // Allow dev.ramxn.com
+      'www.dev.ramxn.com',   // Allow www.dev.ramxn.com
+    ],
+  },
 })
